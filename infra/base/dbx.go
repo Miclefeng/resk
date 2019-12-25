90b05f4fca29fbb045c5350c40d9e3db22161301
miclefeng <miclefengzss@gmail.com>
2019-12-25 17:14:01 +0800

    edit

---
+++ infra/base/dbx.go
@@ -0,0 +1,13 @@
+package base
+
+import (
+	"github.com/tietang/dbx"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+/**
+ * Author : MicleFengzss@gmail.com
+ * Time : 2019/12/25 下午5:11
+ */
+
+ var database *dbx.Database
