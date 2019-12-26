fef7d4507da8a9427bbe3d58e009e6b90bc66465
miclefeng <miclefengzss@gmail.com>
2019-12-26 17:04:39 +0800

    edit

--- example/validator.go
+++ example/validator.go
@@ -1,6 +1,13 @@
 package main
 
-import "gopkg.in/go-playground/validator.v9"
+import (
+	"fmt"
+	"github.com/go-playground/universal-translator"
+	"github.com/go-playground/locales/zh"
+	"gopkg.in/go-playground/validator.v9"
+	"gopkg.in/go-playground/validator.v9/translations/zh"
+)
+
 /**
  * Author : MicleFengzss@gmail.com
  * Time : 2019/12/26 下午3:46
@@ -13,13 +20,28 @@ type Users struct {
 	Email     string `validate:"required,email"`
 }
 
-func main()  {
+func main() {
 	user := &Users{
 		FirstName: "zhang",
-		LastName: "ShangShang",
-		Age: 27,
-		Email: "miclefengzss@163.com",
+		LastName:  "ShangShang",
+		Age:       -1,
+		Email:     "miclefengzss163.com",
 	}
 
 	validate := validator.New()
+
+	cn := zh.New()
+	uni := ut.New(cn, cn)
+
+	err := validate.Struct(user)
+	if err != nil {
+		_, ok := err.(*validator.InvalidValidationError)
+		if ok {
+			fmt.Println(err)
+		}
+		errors, ok := err.(validator.ValidationErrors)
+		if ok {
+			fmt.Println(errors)
+		}
+	}
 }
