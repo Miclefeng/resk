38e836a9924747fc4c112feff67f94095ea85606
miclefeng <miclefengzss@gmail.com>
2019-12-26 11:02:04 +0800

    edit

--- bootstrap/starter/log.go
+++ bootstrap/starter/log.go
@@ -3,7 +3,8 @@ package starter
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/x-cray/logrus-prefixed-formatter"
-	"os"
+	"github.com/rifflock/lfshook"
+	"github.com/lestrrat-go/file-rotatelogs"
 )
 
 /**
@@ -13,17 +14,26 @@ import (
 
 func init()  {
 	// 日志格式
-	formatter := &logrus.TextFormatter{}
-	formatter.FullTimestamp = true
-	formatter.TimestampFormat = "2006-01-02 15:04:05.000"
+	formatter := &prefixed.TextFormatter{
+		FullTimestamp:   true,
+		ForceFormatting: true,
+		TimestampFormat: "2006-01-02.15:04:05.000000",
+		ForceColors:     true,
+		DisableColors:   false,
+	}
 	logrus.SetFormatter(formatter)
 	// 日志级别
-	level := os.Getenv("log.level")
-	if level == "true" {
+	//level := os.Getenv("log.level")
+	//if level == "true" {
 		logrus.SetLevel(logrus.DebugLevel)
-	}
+	//}
 	// 控制台高亮显示
-	formatter.ForceColors = true
-	formatter.DisableColors = false
+	formatter.SetColorScheme(&prefixed.ColorScheme{
+		InfoLevelStyle: "green",
+		DebugLevelStyle: "36",
+		WarnLevelStyle: "yellow",
+		TimestampStyle: "37",
+	})
 	// 日志文件和滚动配置
+	logrus.Debug("testing")
 }
